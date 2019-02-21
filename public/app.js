91cde81f8ae522a24d2de1ea47d49e8f1777f688
Federico Madaffari <icomad@me.com>
2019-02-21 03:10:27 +0100

    fixed api url

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 const fetchData = async () => {
-  const apiURL = 'http://104.248.246.88:8000/dataset';
+  const apiURL = 'http://nba-rpm.icomad.me:8000/dataset';
   const dataset = await axios.get(apiURL);
   return dataset.data;
 }
